c008dfc712888c96520bbad58622199e6725fd85
Sravanti Aketi <sravanti.aketi@gmail.com>
2020-01-22 16:25:39 +1100

    Latest changes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,37 @@
-import  React  from "react";
+import  React, {Component}  from "react";
 import Bar from '../../hoc/Bar'
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-   
-   <Bar>
-        <div>Sidebar</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-   </Bar>
-);
+class Layout extends Component {
 
+    state = {
+        showSideDrawer : false
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer : false});
+    }
+
+    sideDrawerToggleClicked = ()  => {
+        this.setState((prevState) => {
+           return { showSideDrawer : !prevState.showSideDrawer };
+        });
+    }
+
+    render() {
+        return (<Bar>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleClicked}/>
+            <SideDrawer 
+            open = {this.state.showSideDrawer} 
+            closed = {this.sideDrawerClosedHandler}
+            />
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+       </Bar>);
+    }
+}  
+
+export default Layout;
